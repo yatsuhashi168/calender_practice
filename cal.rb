f8675c7c954a606af9db6ffcf4594499c6098f0d
yatsuhashi <happy.32104luk@gmail.com>
2020-12-16 18:33:19 +0900

    日にち表示のプログラムを修正

--- cal.rb
+++ cal.rb
@@ -9,37 +9,34 @@ year = date.year
 require 'optparse'
 opt = OptionParser.new
 
-opt.on('-y VAL', 'set a year') {|v| year = v.to_i}
-opt.on('-m VAL', 'set a month') {|v| month = v.to_i}
+opt.on('-y VAL', 'set a year') { |v| year = v.to_i }
+opt.on('-m VAL', 'set a month') { |v| month = v.to_i }
 
 opt.parse!(ARGV)
 
-puts "#{month}月 #{year}".center(10)
+puts "#{month}月 #{year}".center(18)
 weekdays = %w[日 月 火 水 木 金 土]
 weekdays.each do |weekday|
   print weekday.center(2)
 end
 puts
+
 firstday = Date.new(year, month, 1)
 lastday = Date.new(year, month, -1)
-countday = firstday - 7
+countday = firstday
+brankday = firstday
+
 while countday.day != lastday.day
 
-  while countday.month != lastday.month
-    if countday.wday.zero?
-      while countday.month != lastday.month
-        print ''.center(3)
-        countday += 1
-      end
-    else
-      countday += 1
-    end
+  while brankday.wday != 0
+    print ''.center(3)
+    brankday -= 1
   end
 
   print countday.day.to_s.center(3)
-
   puts if countday.wday == 6
 
   countday += 1
 end
 
+puts
